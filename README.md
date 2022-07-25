54d6d5014f94c5249ada8fa4dcd3044f43e9d3f5
mariarykhlik <maria.rykhlik@gmail.com>
2022-07-25 15:25:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/qf7wp489d5raroux?svg=true)](https://ci.appveyor.com/project/mariarykhlik/aqa-api-ci-postmanecho)
---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void test() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("RUB") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("RUR"))
+        ;
+    }
+}
